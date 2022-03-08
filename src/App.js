ef6f0fe850218c4dc2271433dbb22ef3b6988f1c
Jones-bass <jonesbass.tb@gmail.com>
2022-03-08 19:41:05 -0300

    Organizando o Context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import HomePage from "./pages/HomePage";
 import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
 import './App.css';
 
 
@@ -9,11 +10,13 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+      <UserStorage>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login/*" element={<Login />} />
           <Route path="/visitante/" element={<HomePage />} />
         </Routes>
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,52 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
-import { TOKEN_POST, USER_GET } from '../../api';
+import { UserContext } from "../../UserContext";
+import { ReactComponent as LogoMarca } from "../../assets/tudoo.svg";
+import "../../styles/Header.css";
+
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if (token)
-      getUser(token);
-  }, [])
-
-  async function getUser(token) {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  }
+  const { userLogin, data } = useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
-
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      })
-
-      const response = await fetch(url, options)
-      const json = await response.json();
-      window.localStorage.setItem('token', json.token);
-      getUser(json.token);
+      userLogin(username.value, password.value);
     }
   }
 
   return (
     <section>
+      <header className="header">
+        <Link className="logo" to="/" aria-label="Home">
+          <LogoMarca />
+        </Link>
+        <Link to="/"></Link>
+        {data ? (
+          <Link className="login" to="/visitante">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className="login" to="/criar">
+            Criar
+          </Link>
+        )}
+      </header>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
-      <Link to="/login/criar">Cadastro</Link>
     </section>
   );
 };
