6fb11d81241705e23d7031e18a78c82a8cc7814d
jones-bass <jonesbass.tb@gmail.com>
2022-04-27 21:22:35 -0300

    Icons

--- src/pages/LandPage/LandPage.js
+++ src/pages/LandPage/LandPage.js
@@ -5,6 +5,9 @@ import imgAzulejo from '../../assets/azulejo.jpg';
 import img1 from '../../assets/img1.jpg';
 import img2 from '../../assets/img2.jpg';
 
+import { FaChevronDown, FaChild, FaLayerGroup } from "react-icons/fa";
+
+
 import Post from './Post';
 import styles from './styles.module.scss';
 
@@ -36,22 +39,22 @@ const LandPage = () => {
       </div>
 
       <div className={styles.container}>
-        <h1>SOBRE NÓS</h1>
+        <h1><FaChild style={{ marginBottom: '-0.5rem'}}/> SOBRE NÓS</h1>
         <section className={styles.sobre}>
           <div className={styles.sobreInfo}>
-            <h1>TudoTudo Copiadora</h1>
+            <h1><FaLayerGroup style={{ marginBottom: '-0.4rem'}}/> TudoTudo Copiadora</h1>
             <p>Somos uma empresa especializadas em serviços gráficos há mais de 08 anos aqui no Centro-Sul
               de Sergipe, somos líder em atendimento, trabalhamos com responsabilidade, credibilidade e satisfação
               levando com todo carinho aos nossos clientes e amigos.</p>
             <h2>Trabalhamos</h2>
-              <li>* Xerox - Impressões</li>
-              <li>* Plastificação</li>
-              <li>* Encadernação</li>
-              <li>* Consultas de Veículos</li>
-              <li>* Consultas CPF</li>
-              <li>* Fardamentos</li>
-              <li>* Sublimação em Geral</li>
-              <li>* Personalizados em Geral</li>
+              <li><FaChevronDown className={styles.fa}/> Xerox - Impressões</li>
+              <li><FaChevronDown className={styles.fa}/> Plastificação</li>
+              <li><FaChevronDown className={styles.fa}/> Encadernação</li>
+              <li><FaChevronDown className={styles.fa}/> Consultas de Veículos</li>
+              <li><FaChevronDown className={styles.fa}/> Consultas CPF</li>
+              <li><FaChevronDown className={styles.fa}/> Fardamentos</li>
+              <li><FaChevronDown className={styles.fa}/> Sublimação em Geral</li>
+              <li><FaChevronDown className={styles.fa}/> Personalizados em Geral</li>
           </div>
           <div className={styles.sobreImg}>
             <img className={styles.imgPersonalizados} src={img1} alt="Personalizados" />
