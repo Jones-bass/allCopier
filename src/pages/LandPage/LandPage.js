197802ca2489a995335ec54ecce72984334884fd
jones-bass <jonesbass.tb@gmail.com>
2022-09-10 22:19:57 -0300

    Json-Server

--- src/pages/LandPage/LandPage.js
+++ src/pages/LandPage/LandPage.js
@@ -1,76 +1,122 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import img1 from '../../assets/img1.jpg';
-import img2 from '../../assets/img2.jpg';
+import img1 from "../../assets/img1.jpg";
+import img2 from "../../assets/img2.jpg";
 
-import { FaChevronDown, FaChild, FaLayerGroup } from "react-icons/fa";
-import Post from './Post';
-import styles from './styles.module.css';
+import { FaChevronDown, FaChild, FaLayerGroup, FaProductHunt } from "react-icons/fa";
 
-const post = [
+import styles from "./styles.module.css";
+
+const icons = [
   {
-    title: 'Caneca Personalizada',
-    text: 'Criamos sua caneca também! e vamos presentear aquela pessoal ESPECIAL.',
-    image: 'https://http2.mlstatic.com/D_NQ_NP_858781-MLB31777794699_082019-O.jpg'
+    text: "Xerox - Impressões",
   },
   {
-    title: 'Camiseta Personalizada',
-    text: 'Crie sua CAMISA AGORA! Solte a sua imaginação. deixe que a gente faz o resto.',
-    image: 'https://img.elo7.com.br/product/zoom/2970630/30-camisetas-personalizada-todos-os-temas-camisa-festa-aniv-sublimacao.jpg'
+    text: "Plastificação",
   },
   {
-    title: 'Almofada Personalizada',
-    text: 'Crie sua ALMOFADA! Solte a sua imaginação. A gente faz o resto.',
-    image: 'https://images.tcdn.com.br/img/img_prod/650501/almofada_personalizada_mickey_tradicional_20x30_f_e_v_101_1_20211227090444.jpg'
+    text: "Encadernação ",
   },
   {
-    title: 'Azulejo Personalizada',
-    text: 'Criamos AZULEJO PERSONALIZADOS! Presentei quem você mais ama.',
-    image: 'https://cdn.awsli.com.br/1000x1000/1221/1221152/produto/97497177/f9c9f6a79b.jpg'
-  }
-]
+    text: "Consultas de Veículos",
+  },
+  {
+    text: "Consultas CPF",
+  },
+  {
+    text: "Fardamentos",
+  },
+  {
+    text: "Sublimação em Geral ",
+  },
+  {
+    text: "Personalizados em Geral",
+  },
+];
+
 const LandPage = () => {
-  return (
+  const [posts, setPosts] = useState([]);
 
+  async function loadPosts() {
+    const response = await fetch("http://localhost:3333/posts");
+    const data = await response.json();
+
+    setPosts(data);
+  }
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
+  return (
     <div>
       <div>
-        {post.map(item => (
-          <Post
-            title={item.title}
-            text={item.text}
-            image={item.image}
-          />
+        {posts.map((post, index) => (
+          <div key={index}>
+            <div className={styles.containerConteudo}>
+              <div className={styles.containerText}>
+                <span className={styles.detalhe}></span>
+                <h1 className={styles.title}>{post.title}</h1>
+                <p className={styles.text}>{post.text}</p>
+                <button>
+                  <FaProductHunt 
+                  />
+                  PERSONALIZE!
+                </button>
+              </div>
+              <img
+                className={styles.imgPersonalizados}
+                src={post.image}
+                alt="Personalizados"
+              />
+            </div>
+          </div>
         ))}
       </div>
 
       <div className={styles.containerPost}>
-        <h1><FaChild style={{ marginBottom: '-0.5rem' }} /> SOBRE NÓS</h1>
+        <h1>
+          <FaChild style={{ marginBottom: "-0.5rem" }} /> SOBRE NÓS
+        </h1>
         <section className={styles.sobre}>
           <div className={styles.sobreInfo}>
-            <h1><FaLayerGroup style={{ marginBottom: '-0.4rem' }} /> TudoTudo Copiadora</h1>
-            <p>Somos uma empresa especializadas em serviços gráficos há mais de 08 anos aqui no Centro-Sul
-              de Sergipe, somos líder em atendimento, trabalhamos com responsabilidade, credibilidade e satisfação
-              levando com todo carinho aos nossos clientes e amigos.</p>
+            <h1>
+              <FaLayerGroup style={{ marginBottom: "-0.4rem" }} /> TudoTudo
+              Copiadora
+            </h1>
+            <p>
+              Somos uma empresa especializadas em serviços gráficos há mais de
+              08 anos aqui no Centro-Sul de Sergipe, somos líder em atendimento,
+              trabalhamos com responsabilidade, credibilidade e satisfação
+              levando com todo carinho aos nossos clientes e amigos.
+            </p>
             <h2>Trabalhamos</h2>
-            <li><FaChevronDown className={styles.fa} /> Xerox - Impressões</li>
-            <li><FaChevronDown className={styles.fa} /> Plastificação</li>
-            <li><FaChevronDown className={styles.fa} /> Encadernação</li>
-            <li><FaChevronDown className={styles.fa} /> Consultas de Veículos</li>
-            <li><FaChevronDown className={styles.fa} /> Consultas CPF</li>
-            <li><FaChevronDown className={styles.fa} /> Fardamentos</li>
-            <li><FaChevronDown className={styles.fa} /> Sublimação em Geral</li>
-            <li><FaChevronDown className={styles.fa} /> Personalizados em Geral</li>
+            {icons.map((icon, index) => (
+              <li key={index}>
+                <FaChevronDown className={styles.fa} />
+                {icon.text}
+              </li>
+            ))}
+          </div>
+        
+          <div className={styles.sobreImg}>
+            <img
+              className={styles.imgPersonalizados}
+              src={img1}
+              alt="Personalizados"
+            />
           </div>
           <div className={styles.sobreImg}>
-            <img className={styles.imgPersonalizados} src={img1} alt="Personalizados" />
-          </div>
-          <div className={styles.sobreImg}>
-            <img className={styles.imgPersonalizados} src={img2} alt="Personalizados" />
+            <img
+              className={styles.imgPersonalizados}
+              src={img2}
+              alt="Personalizados"
+            />
           </div>
         </section>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LandPage;
